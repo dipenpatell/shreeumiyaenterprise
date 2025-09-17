188f1451c0b7cce8c26f896dbeebcd42ab032477
dipen patel <dipenpatel0243@gmail.com>
2025-09-17 11:46:58 +0530

    lateste

--- flotbit-boilerplate/frontend/src/common/layout/vertical_layout/VerticalLayout.jsx
+++
@@ -1,9 +0,0 @@
-const VerticalLayout = (props) => {
-  return (
-    <div className="vertical-layout" id="layout-wrapper">
-      <div className="main-content">{props.children}</div>
-    </div>
-  );
-};
-
-export default VerticalLayout;
--- flotbit-boilerplate/frontend/src/common/services/GenrateRandomId.js
+++
@@ -1,4 +0,0 @@
-export const generateRandomId = () => {
-  // Example: 8-character alphanumeric
-  return Math.random().toString(36).substring(2, 10);
-};
