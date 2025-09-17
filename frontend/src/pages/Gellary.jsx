aca91b65647665c3c861e63875e8a323d2adaea0
dipen patel <dipenpatel0243@gmail.com>
2025-09-17 13:47:41 +0530

    gird

--- frontend/src/pages/Gellary.jsx
+++ frontend/src/pages/Gellary.jsx
@@ -69,7 +69,7 @@ export default function PhotographyGallery() {
         </div>
 
         {/* Gallery Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mb-16">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mb-16">
           {visibleItems.map((item, index) => (
             <div
               key={index}
